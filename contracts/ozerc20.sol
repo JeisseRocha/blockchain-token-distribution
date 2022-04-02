3621ec6c4082493b8c025f88eceff88215f36d87
Jeisse Rocha <x21129924@student.ncirl.ie>
2022-04-02 14:53:57 +0100

    updated contract

--- contracts/ozerc20.sol
+++ contracts/ozerc20.sol
@@ -1,5 +1,11 @@
+/**
+ *Submitted for verification at Etherscan.io on 2022-02-20
+*/
+
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.4.1 (token/ERC20/ERC20.sol)
+
+// OpenZeppelin Contracts v4.4.1 (token/ERC20/IERC20.sol)
 
 pragma solidity ^0.8.0;
 
@@ -7,43 +13,77 @@ pragma solidity ^0.8.0;
  * @dev Interface of the ERC20 standard as defined in the EIP.
  */
 interface IERC20 {
-
-     //Returns the amount of tokens in existence.
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
     function totalSupply() external view returns (uint256);
 
-    //Returns the amount of tokens owned by `account`.
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
     function balanceOf(address account) external view returns (uint256);
 
-
-    //Moves `amount` tokens from the caller's account to `recipient` and return true if operation succeeded
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
     function transfer(address recipient, uint256 amount) external returns (bool);
 
-
-    // Returns the remaining number of tokens that `spender` will be
-    // allowed to spend on behalf of `owner` through {transferFrom}. This is
-    // zero by default.
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
     function allowance(address owner, address spender) external view returns (uint256);
 
-
-    // Sets `amount` as the allowance of `spender` over the caller's tokens and return true if operation succeeded
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
     function approve(address spender, uint256 amount) external returns (bool);
 
-
-    // Moves `amount` tokens from `sender` to `recipient` using the allowance mechanism
-    // `amount` is then deducted from the caller's allowance
-    // Returns true if the operation succeeded
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
     function transferFrom(
         address sender,
         address recipient,
         uint256 amount
     ) external returns (bool);
 
-
-    // Emitted when `value` tokens are moved from one account (`from`) to another (`to`).
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    // Emitted when the allowance of a `spender` for an `owner` is set by
-    // a call to {approve}. `value` is the new allowance.
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
@@ -140,20 +180,24 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * All two of these values are immutable: they can only be set once during
      * construction.
      */
-    //string memory name_, string memory symbol_
-    constructor() {
-        _name = "NEW TOKEN ON BLOCKCHAIN CLASS v1";
-        _symbol = "NTO";
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
 
         _mint(msg.sender, 1000000000000000000000000);
     }
 
-    // Returns the name of the token.
+    /**
+     * @dev Returns the name of the token.
+     */
     function name() public view virtual override returns (string memory) {
         return _name;
     }
 
-    // Returns the symbol of the token, usually a shorter version of the name
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
     function symbol() public view virtual override returns (string memory) {
         return _symbol;
     }
@@ -175,12 +219,16 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         return 18;
     }
 
-    // See {IERC20-totalSupply}
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
     function totalSupply() public view virtual override returns (uint256) {
         return _totalSupply;
     }
 
-    // See {IERC20-balanceOf}
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
     function balanceOf(address account) public view virtual override returns (uint256) {
         return _balances[account];
     }
@@ -198,7 +246,9 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         return true;
     }
 
-    // See {IERC20-allowance}
+    /**
+     * @dev See {IERC20-allowance}.
+     */
     function allowance(address owner, address spender) public view virtual override returns (uint256) {
         return _allowances[owner][spender];
     }
